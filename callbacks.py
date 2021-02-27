f2acd3ba2fa15cb4ad6fd08908e40eb31f4b07d4
Arne Krüger <kruegerarneak@gmail.com>
2021-02-27 14:05:14 +0100

    first test

--- callbacks.py
+++ callbacks.py
@@ -79,9 +79,9 @@ class Callbacks(object):
         for forward_name in self.config.forwards:
             if self.config.forwards[forward_name]['from'] == room.display_name:
                 logger.debug(
-                    f"will forward to :{self.config.forwards[forward_name]['to']} | "
+                    f"will forward to :{self.config.forwards[forward_name]['to']} | {str(event)}"
                 ) 
-                await send_text_to_room(self.client, self.config.forwards[forward_name]['to'], event.body, notice=False)
+                await send_text_to_room(self.client, self.config.forwards[forward_name]['to'], event.body, notice=True)
         logger.debug(
             f"Bot message received for room {room.display_name} | "
             f"{room.user_name(event.sender)}: {msg}"
